1674454699415d5fd29bbb156456ddac2916b346
Yun000 <126633249+Yun000@users.noreply.github.com>
2023-05-26 19:59:14 +0200

    Add files via upload

---
+++ CY-Shop/management_system.h
@@ -0,0 +1,10 @@
+#ifndef MANAGEMENT_SYSTEM_H
+#define MANAGEMENT_SYSTEM_H
+
+#include "store.h"
+#include "common.h"
+
+// Affichage du mode gestion
+void management_system_menu(Store* store);
+
+#endif
